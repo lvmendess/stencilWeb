9e3aa1e5d0f49d140e20d40d0d2b8ac6046e69b9
lvmendess <liviamendes1703@gmail.com>
2025-05-16 18:51:18 -0300

    servindo htmly

--- src/main/java/com/stencilwebclient/stencilweb/controller/CharacterController.java
+++ src/main/java/com/stencilwebclient/stencilweb/controller/CharacterController.java
@@ -2,24 +2,40 @@ package com.stencilwebclient.stencilweb.controller;
 
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stencilwebclient.stencilweb.CharacterRepository;
+import com.stencilwebclient.stencilweb.service.CharacterService;
 import com.stencilwebclient.stencilweb.Character;
 
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController
+@Controller
 public class CharacterController {
 
     private final CharacterRepository repo;
+    private final CharacterService service;
     
-    public CharacterController(CharacterRepository repo){
+    /*public CharacterController(CharacterRepository repo){
+        this.repo = repo;
+    }*/
+
+    public CharacterController(CharacterService service, CharacterRepository repo){
+        this.service = service;
         this.repo = repo;
     }
+
+    @GetMapping("/home")
+    public String listCharacters(Model model) {
+        model.addAttribute("characters", service.getAllCharacters());
+        return "index";
+    }
+    
 
     @GetMapping("/info")
     public String info(){
---
+++ src/main/java/com/stencilwebclient/stencilweb/service/CharacterService.java
@@ -0,0 +1,20 @@
+package com.stencilwebclient.stencilweb.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.stencilwebclient.stencilweb.CharacterRepository;
+
+@Service
+public class CharacterService {
+    private final CharacterRepository repo;
+
+    public CharacterService(CharacterRepository repo){
+        this.repo = repo;
+    }
+
+    public List<com.stencilwebclient.stencilweb.Character> getAllCharacters(){
+        return repo.findAll();
+    }
+}
