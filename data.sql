5ebbc507bee74537ddb773dfa4e70be7691b0e6e
lvmendess <liviamendes1703@gmail.com>
2025-07-06 16:45:28 -0300

    mudanças data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 USE Stencil;
-INSERT INTO Aluno (nomeAluno, xp, skin) VALUES ('aluno1', '', 100, 'fabiola.png', 0),
+INSERT INTO Aluno (nomeAluno, nick, xp, skin, ofensiva) VALUES ('aluno1', '', 100, 'fabiola.png', 0),
 ('aluno2', '', 100, 'anaLuzia.png', 0),
 ('aluno3', '', 100, 'bernardo.png', 0),
 ('aluno5', '', 100, 'gabrielHenrique.png', 0),
@@ -10,4 +10,4 @@ INSERT INTO Aluno (nomeAluno, xp, skin) VALUES ('aluno1', '', 100, 'fabiola.png'
 INSERT INTO Turma (nomeTurma) VALUES ('turmaA'), ('turmaB');
 
 #UPDATE Characters SET skin = 'anaLuzia.png' WHERE id=2;
-#SELECT * FROM Characters;
+#SELECT * FROM Aluno;
