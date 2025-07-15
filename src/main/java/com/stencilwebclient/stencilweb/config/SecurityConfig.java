4028a791865cd7056c3af45931c4e282382231d7
lvmendess <liviamendes1703@gmail.com>
2025-07-14 23:05:42 -0300

    procrastinei :D

--- src/main/java/com/stencilwebclient/stencilweb/config/SecurityConfig.java
+++ src/main/java/com/stencilwebclient/stencilweb/config/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.stencilwebclient.stencilweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.stencilwebclient.stencilweb.service.StencilUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -12,6 +17,31 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/error").permitAll()
+                        .anyRequest().authenticated()
+                    )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/home", true).permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")    
+                )
+                .build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(
+        StencilUserDetailsService userDetailsService, PasswordEncoder passwordEncoder){
+            DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+            provider.setUserDetailsService(userDetailsService);
+            provider.setPasswordEncoder(passwordEncoder);
+            return provider;
     }
 }
