d4586418c51d54bb88c1976b48702772a1429ccb
lvmendess <liviamendes1703@gmail.com>
2025-09-12 22:05:59 -0300

    query usuarios sem aluno associado

--- src/main/java/com/stencilwebclient/stencilweb/repository/AlunoRepository.java
+++ src/main/java/com/stencilwebclient/stencilweb/repository/AlunoRepository.java
@@ -1,6 +1,9 @@
 package com.stencilwebclient.stencilweb.repository;
 
 import com.stencilwebclient.stencilweb.models.Aluno;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +15,6 @@ public interface AlunoRepository extends JpaRepository<Aluno, Integer>{
     @Query("SELECT a.idAluno FROM Aluno a WHERE a.user.idUsuario = :userId")
     Integer findAlunoIdByIdUsuario(@Param("userId") Integer userId);
 
+    @Query("SELECT u.nomeUsuario FROM Usuario u LEFT JOIN Aluno a ON a.usuario_id_s = u.idUsuario WHERE a.usuario_id_s IS NULL AND u.roleUsuario = 'S';")
+    List<String> findUsuariosWithoutAluno();
 }
